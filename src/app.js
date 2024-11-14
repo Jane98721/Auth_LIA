f9ea58926c9335774baad3122cf8fe2afe96e057
J L <jl-@live.se>
2024-11-14 13:11:35 +0100

    Uninstalled helmet

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@
 const express = require ('express')
 const authRoutes = require('./Routes/auth_routes')
 const app = express ()
-const helmet = require('helmet')
 
-app.use(helmet())
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 
