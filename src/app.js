3863faa60c161297cda238dfe77091016c598ebe
J L <jl-@live.se>
2024-11-14 10:36:18 +0100

    Installed helmet

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@
 const express = require ('express')
 const authRoutes = require('./Routes/auth_routes')
 const app = express ()
+const helmet = require('helmet')
 
+app.use(helmet())
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 
